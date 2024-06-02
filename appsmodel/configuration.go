454a88853d18fbe8394dcd5ee921b4ed79755269
agungdhewe <agung_dhewe@yahoo.com>
2024-06-02 12:46:42 +0700

    tambah configurasi

--- appsmodel/configuration.go
+++ appsmodel/configuration.go
@@ -1,5 +1,27 @@
 package appsmodel
 
 type Configuration struct {
-	Port int `yaml:"port"`
+	Port     int `yaml:"port"`
+	Database struct {
+		Name     string `yaml:"name"`
+		Server   string `yaml:"server"`
+		UserName string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"database"`
+	SecureCookie bool `yaml:"securecookie"`
+	Cookie       struct {
+		Secure   string `yaml:"secure"`
+		LifeTime int    `yaml:"lifetime"`
+	} `yaml:"cookie"`
+	Template struct {
+		Cached bool   `yaml:"cached"`
+		Dir    string `yaml:"dir"`
+	} `yaml:"template"`
+	Application struct {
+		PageDir    string `yaml:"pagedir"`
+		ContentDir string `yaml:"contentdir"`
+	} `yaml:"application"`
+	Logging struct {
+		Enabled bool `yaml:"enabled"`
+	} `yaml:"logging"`
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fgtago/fgweb
 
 go 1.22.3
 
-require github.com/agungdhewe/dwlog v1.2.3 // indirect
+require github.com/agungdhewe/dwlog v1.2.3
