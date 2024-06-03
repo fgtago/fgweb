60ea482c43f62ca37f159a31a1e2d4889e46b6a9
agungdhewe <agung_dhewe@yahoo.com>
2024-06-03 16:24:55 +0700

    page handler bisa akses template manager yang ada di ws

--- main/router.go
+++ main/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/fgtago/fgweb"
+	"github.com/fgtago/fgweb/appsmodel"
 	"github.com/fgtago/fgweb/main/apps"
 	"github.com/go-chi/chi/v5"
 )
@@ -17,9 +18,21 @@ func Router(mux *chi.Mux) error {
 }
 
 func pagehandlerHome(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	app := apps.GetApplication()
+	device := ctx.Value(appsmodel.DeviceKeyName).(appsmodel.Device)
 
-	app.Webservice.TplMgr.Ready()
+	// TODO: implmentasikan tpl
+	tpl, exists, err := app.Webservice.TplMgr.GetPage("homes", device.Type)
+	if err != nil {
+		// error 500
+		fmt.Println(err.Error())
+	}
+
+	if !exists {
+		// error 404
+		fmt.Println("404")
+	}
 
 	fmt.Fprintln(w, "home page nya ok", app.Webservice.Configuration.Port)
 }
